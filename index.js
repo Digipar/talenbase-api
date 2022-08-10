b115af929e6f5b2b6f93fec20511dbfa176305c1
Victor Martinetti <victor.martinetti@digipar.com>
2022-08-10 15:41:24 -0400

    Moved to 8080

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const path = require("path");
-// const express = require("express");
 
-
-
-// const app = express();
 const app = require("./app")
 
 const mongoConnect = require("./util/database").mongoConnect;
 
-
-// app.use(express.static(path.join(__dirname, "public")));
-app.listen(3001, () => {
+app.listen(8080, () => {
   console.log("Talenbase connected, API is listening on port 3001");
 });
 
