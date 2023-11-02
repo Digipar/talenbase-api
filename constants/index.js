304ec96aaca89156ec1d559d74f1bb079b0c7aa8
Dev06 <dev06@digipar.com>
2023-11-02 09:52:31 -0300

    UPLOAD para cambiar de rama

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,20 @@
 import cryptoJs from "crypto-js";
-import { APP_HOST } from "../config/config.js"
+import { APP_HOST,EMPRESA } from "../config/config.js"
 
-const buildLink = (email) => {
+const buildLinkReset = (email) => {
+  const fechaDDMMYYYY = new Date().toLocaleDateString('es-ES', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+  console.log('fechaDDMMYYYY', fechaDDMMYYYY)
+  const hashedEmail = cryptoJs.MD5(`${email}${fechaDDMMYYYY}`).toString();
+  const link = new URL(`https://${APP_HOST}/reset-password`);
+  link.searchParams.append('hash', hashedEmail);
+  link.searchParams.append('email', email);
+  return link;
+};
+const buildLinkActivate = (email) => {
   const fechaDDMMYYYY = new Date().toLocaleDateString('es-ES', {
     day: '2-digit',
     month: '2-digit',
@@ -16,7 +29,7 @@ const buildLink = (email) => {
 };
 
 export const getPasswordResetText = (email) => {
-  const link = buildLink(email);
+  const link = buildLinkReset(email);
   return `
 Por favor use el siguiente link para cambiar su contraseña: 
 ${link}
@@ -25,9 +38,9 @@ Por favor, no responda a este correo electrónico.
 
 Atentamente,
 
-Equipo de soporte de Agrofertil`};
+Equipo de soporte de ${EMPRESA}`};
 export const getPasswordResetHtml = (email) => {
-  const link = buildLink(email);
+  const link = buildLinkReset(email);
   return `
   Por favor use el siguiente link para cambiar su contraseña: <br/>
   ${link} <br/>
@@ -36,6 +49,25 @@ export const getPasswordResetHtml = (email) => {
   <br/>
   Atentamente,<br/>
   
-  Equipo de soporte de Agrofertil`;
+  Equipo de soporte de ${EMPRESA}`;
+}
+
+export const getActivateAccountText = (email) => {ß
+  const link = buildLinkActivate(email);
+  return `
+Por favor use el siguiente link para activar su cuenta:
+${link} 
+Por favor, no responda a este correo electrónico.
+Atentamente,
+Equipo de soporte de ${EMPRESA}.`
+}
+export const getActivateAccountHtml = (email) => {
+  const link = buildLinkActivate(email);
+  return `
+  Por favor use el siguiente link para activar su cuenta: <br/>
+  ${link} <br/>
+  Por favor, no responda a este correo electrónico.<br/>
+  Atentamente,<br/>
+  Equipo de soporte de ${EMPRESA}.`
 }
 
--- controllers/mailer.js
+++ controllers/mailer.js
@@ -1,7 +1,7 @@
 import { SPR } from "../connection/connection.js";
-import { SHAREPOINT_API, SITE_URL, SENDGRID_API_KEY,NO_REPLY_EMAIL } from "../config/config.js";
+import { SHAREPOINT_API, SENDGRID_API_KEY,NO_REPLY_EMAIL,EMPRESA } from "../config/config.js";
 import sgMail from '@sendgrid/mail';
-import {getPasswordResetHtml, getPasswordResetText} from '../constants/index.js';
+import {getActivateAccountHtml, getActivateAccountText, getPasswordResetHtml, getPasswordResetText} from '../constants/index.js';
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 const registerMailerActivateAccount = async (req, res) => {
@@ -40,13 +40,9 @@ const registerMailerActivateAccount = async (req, res) => {
     const msg = {
       to: email,
       from: 'noreply@digipar.com',
-      subject: 'Activación de cuenta de AGROFERTIL [Talenbase]',
-      text: `
-      Por favor use el siguiente link para activar su cuenta:
-      `,
-      html: `
-      Por favor use el siguiente link para activar su cuenta: <br/>
-      `
+      subject: `Activación de cuenta de ${EMPRESA} [Talenbase]`,
+      text: getActivateAccountText(email),
+      html: getActivateAccountHtml(email)
     };
     try {
       sgMail.send(msg);
@@ -98,7 +94,7 @@ const registerMailerResetPass = async (req, res) => {
   const msg = {
     to: email,
     from: NO_REPLY_EMAIL,
-    subject: 'Cambio de contraseña de AGROFERTIL [Talenbase]',
+    subject: 'Cambio de contraseña de Talenbase ',
     text: getPasswordResetText(email),
     html: getPasswordResetHtml(email),
   };
